e1de1a45f76e3de3e87e1485363f739ac7b8ea8d
Julia Sanchis <jursanchis@gmail.com>
2024-01-12 10:24:51 +0100

    commit cats

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const dogsUrl = "https://dog.ceo/api/breeds/image/random/10";
 const catsUrl = "https://api.thecatapi.com/v1/images/search?limit=10";
+
+const catsDiv = document.getElementById("catsDiv");
+const btnCats = document.querySelector(".cats");
+
+const getCatsImgs = function () {
+  fetch(catsUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((element) => {
+        const image = document.createElement("img");
+        image.src = element.url;
+        catsDiv.append(image);
+      });
+    });
+};
+
+btnCats.addEventListener("click", getCatsImgs);
